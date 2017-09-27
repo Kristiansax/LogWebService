6f7d54c181d14f54fe92b05fdb046e26ad405bf1
Kristiansax <krellesax@hotmail.com>
2017-09-27 10:52:10 +0200

    Setup WCF app and test library

---
+++ LogWebService/LogReaderTests/LogReaderServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogReaderTests
+{
+    [TestClass]
+    public class LogReaderServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
