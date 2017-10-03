7d13c98e060d6c5e933da7683facf3bd6b3027af
Kristiansax <krellesax@hotmail.com>
2017-10-03 09:25:16 +0200

    Changed to Web API, added incident class

---
+++ LogReaderService/LogReaderService/Models/Incident.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogReaderService.Models
+{
+    public class Incident
+    {
+        public string time { get; set; }
+        public string ID { get; set; }
+        public string alarm { get; set; }
+        public string name { get; set; }
+        public string afdeling { get; set; }
+        public string residence { get; set; }
+        public string location { get; set; }
+        public string resettime { get; set; }
+        public string personnel { get; set; }
+        public string callto { get; set; }
+        public string groupcall { get; set; }
+    }
+}
--- LogReaderService/LogReaderService/Properties/AssemblyInfo.cs
+++ LogReaderService/LogReaderService/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6d779a36-8e6b-468d-bea1-3a5bf6eaeb8f")]
+[assembly: Guid("26389911-16c6-432d-b52a-13768921529d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
